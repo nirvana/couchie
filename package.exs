30092d6b9a293f4c17025e3349b11abdf0107bff
Nirvana Platform <hello@nirvana.io>
2013-04-03 13:54:46 -0300

    added expm package.exs file

---
+++ package.exs
@@ -0,0 +1,7 @@
+Expm.Package.new(name: "Couchie", description: "An Elixir library for accessing Couchbase 2.0",
+                 version: "0.1", keywords: ["Elixir", "Couchbase", "database", "library"], 
+                 homepage: "www.nirvana.io",
+                 maintainers: [[name: "Nirvana Organization", 
+                                email: "hello@nirvana.io"]],
+                 repositories: [[github: "nirvana/couchie"]])
+    
