5d7937d3a09f76d476c6e8d0817ef4e4e22f4b70
MCRedJay <jay.prince@clutchanalytics.com>
2014-03-03 16:46:13 -0600

    Now points to chitika/cberl, fixed doc typo, added open/3

--- lib/couchie.ex
+++ lib/couchie.ex
@@ -26,22 +26,27 @@ defmodule Couchie do
 	      {ok, <0.XX.0>} #=> successful connection to the named bucket, which you can access using the id "secret"
 			
 			# if your bucket isn't password protected (and isn't default)
-	      Couchie.open(:connection, 10, 'localhost:8091", 'bucket_name')
+	      Couchie.open(:connection, 10, 'localhost:8091', 'bucket_name')
 	      {ok, <0.XX.0>} #=> successful connection to the named bucket, which you can access using the id "application"
 
 	  """
  	def open(name) do  
-		open(name, 100, 'localhost:8091', '')
+		open(name, 10, 'localhost:8091')
  	end
 
  	def open(name, size) do  
-		open(name, size, 'localhost:8091', '')
+		open(name, size, 'localhost:8091')
  	end
 	
+  	def open(name, size, host) do  
+  		IO.puts "Opening #{name}, #{size}, #{host}"
+		:cberl.start_link(name, size, host)
+  	end
+
 	def open(name, size, host, bucket) do  # assume the bucket user and pass are the same as bucket name
 		open(name, size, host, bucket, bucket, bucket)
  	end
-
+  	
   	def open(name, size, host, bucket, username, pass) do  #currently usernames are set to bucket names in this interface.
   		IO.puts "Opening #{name}, #{size}, #{host}, #{username}, #{pass}, #{bucket} "
 		:cberl.start_link(name, size, host, username, pass, bucket)
