cda6e6f54f1061ea0671a9e9ce2032ab5a055da6
MCRedJay <jay.prince@clutchanalytics.com>
2014-04-09 17:18:35 -0500

    jazz now returns atoms

--- lib/transcoder.ex
+++ lib/transcoder.ex
@@ -1,6 +1,6 @@
 defmodule Couchie.Transcoder do
   require Bitwise
-  
+
   @cbe_json 0x02
   @cbe_raw 0x04
   @cbe_str 0x08
@@ -10,15 +10,15 @@ defmodule Couchie.Transcoder do
   def encode_value(flags, value) do
     JSON.encode!(value)
   end
-  
+
   def decode_value(flags, value) do
-    JSON.decode!(value)
+    JSON.decode!(value, keys: :atoms)
   end
 
   def flag(encoders) when is_list(encoders) do
-    List.foldr(encoders, 0, fn (x, acc) -> Bitwise.bor(acc, x) end)  
+    List.foldr(encoders, 0, fn (x, acc) -> Bitwise.bor(acc, x) end)
   end
-  
+
   def flag(encoder) do
     case encoder do
       :standard -> @cbe_json
@@ -26,6 +26,6 @@ defmodule Couchie.Transcoder do
       :raw_binary -> @cbe_raw
       :str -> @cbe_str
     end
-  end 
-  
+  end
+
 end
