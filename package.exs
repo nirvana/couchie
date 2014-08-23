65c451deca39029d2a6882745da626713b5d602e
Nirvana <hello@nirvana.io>
2014-08-23 17:00:18 -0500

    package.exs

--- package.exs
+++ package.exs
@@ -1,7 +1,7 @@
 Expm.Package.new(name: "couchie", description: "An Elixir library for accessing Couchbase 2.0",
-                 version: "0.1", keywords: ["Elixir", "Couchbase", "database", "library"], 
+                 version: "0.1", keywords: ["Elixir", "Couchbase", "database", "library"],
                  homepage: "nirvana.io",
-                 maintainers: [[name: "Nirvana Organization", 
+                 maintainers: [[name: "Nirvana Organization",
                                 email: "hello@nirvana.io"]],
                  repositories: [[github: "nirvana/couchie"]])
     
