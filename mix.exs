0f117e9c725eab5ac18f617d2dd3a5f0291237e4
Courtney Couch <courtney.couch@gmail.com>
2016-04-18 04:22:17 -0700

    using Muut fork of cberl

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule Couchie.Mixfile do
   # {:erlmc, "0.1", git: "https://github.com/n1rvana/erlmc.git"}
   defp deps do
     [
-		  {:cberl, github: "chitika/cberl"}, #chitika is authoritative source
+		  {:cberl, github: "muut/cberl"}, #chitika is authoritative source
       {:poison, ">= 1.2.0"}
     ]
   end
