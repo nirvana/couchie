94a96715c40a9c97e15e6b1048c8071cbd61576d
Courtney Couch <courtney@muut.com>
2016-04-19 05:47:35 -0700

    not using atoms for returned
    atoms are not garbage collected, it's a bit dangerous to convert all hash keys to atoms unless you have a finite set of keys in couchbase. If the data is at all user generated, this essentially results in a memory leak otherwise.

--- lib/transcoder.ex
+++ lib/transcoder.ex
@@ -29,7 +29,7 @@ defmodule Couchie.Transcoder do
     do: decode_value(flag ^^^ @raw_flag, :erlang.binary_to_term(value))
 
   def decode_value(flag, value) when (@json_flag &&& flag) === @json_flag,
-    do: decode_value(flag ^^^ @json_flag, Poison.decode!(value, keys: :atoms))
+    do: decode_value(flag ^^^ @json_flag, Poison.decode!(value))
 
   def decode_value(flag, value) when (@str_flag &&& flag) === @str_flag,
     do: decode_value(flag ^^^ @str_flag, value)
