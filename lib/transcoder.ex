8f4d9ebb717b92887bdd2c606490a2a8a2a2e027
Courtney Couch <courtney.couch@gmail.com>
2016-05-01 10:16:22 -0700

    updating transcoder for common flag support
    Newer libraries use the flags:
    
    json: 0x02 << 24
    raw:  0x03 << 24
    utf8: 0x04 << 24
    
    So updating the transcoder to support these flags for interoperability.
    
    Also I prefered not to use the single line function declarations for readability (personal preference)

--- lib/transcoder.ex
+++ lib/transcoder.ex
@@ -1,38 +1,62 @@
 defmodule Couchie.Transcoder do
   use Bitwise
 
-  @json_flag 0x2
-  @raw_flag  0x4
-  @str_flag  0x8
+  @json_flag 0x02 <<< 24
+  @json_flag_legacy 0x02
+
+  @raw_flag  0x03 <<< 24
+  @raw_flag_legacy 0x04
+
+  @str_flag  0x04 <<< 24
+  @str_flag_legacy 0x08
 
   #API defined by cberl_transcoder.erl
 
   # Encoder
 
-  def encode_value(encoders, value),
-    do: do_encode_value(flag(encoders), value)
+  def encode_value(encoders, value) do
+    do_encode_value(flag(encoders), value)
+  end
 
-  def do_encode_value(flag, value) when (flag &&& @str_flag) === @str_flag,
-    do: do_encode_value(flag ^^^ @str_flag, value)
+  def do_encode_value(flag, value) when (flag &&& @str_flag) === @str_flag do
+    do_encode_value(flag ^^^ @str_flag, value)
+  end
 
-  def do_encode_value(flag, value) when (flag &&& @json_flag) === @json_flag,
-    do: do_encode_value(flag ^^^ @json_flag, Poison.encode!(value))
+  def do_encode_value(flag, value) when (flag &&& @json_flag) === @json_flag do
+    do_encode_value(flag ^^^ @json_flag, Poison.encode!(value))
+  end
 
-  def do_encode_value(flag, value) when (flag &&& @raw_flag) === @raw_flag,
-    do: do_encode_value(flag ^^^ @raw_flag, :erlang.term_to_binary(value))
+  def do_encode_value(flag, value) when (flag &&& @raw_flag) === @raw_flag do
+    do_encode_value(flag ^^^ @raw_flag, :erlang.term_to_binary(value))
+  end
 
   def do_encode_value(_, value), do: value
 
   # Decoder
 
-  def decode_value(flag, value) when (@raw_flag &&& flag) === @raw_flag,
-    do: decode_value(flag ^^^ @raw_flag, :erlang.binary_to_term(value))
+  def decode_value(flag, value) when (@raw_flag &&& flag) === @raw_flag do
+    decode_value(flag ^^^ @raw_flag, :erlang.binary_to_term(value))
+  end
+  def decode_value(flag, value) when (@raw_flag_legacy &&& flag) === @raw_flag_legacy do
+    decode_value(flag ^^^ @raw_flag_legacy, :erlang.binary_to_term(value))
+  end
 
-  def decode_value(flag, value) when (@json_flag &&& flag) === @json_flag,
-    do: decode_value(flag ^^^ @json_flag, Poison.decode!(value))
 
-  def decode_value(flag, value) when (@str_flag &&& flag) === @str_flag,
-    do: decode_value(flag ^^^ @str_flag, value)
+  def decode_value(flag, value) when (@json_flag &&& flag) === @json_flag  do
+    decode_value(flag ^^^ @json_flag, Poison.decode!(value))
+  end
+  def decode_value(flag, value) when (flag &&& @json_flag_legacy) === @json_flag_legacy do
+    decode_value(flag ^^^ @json_flag_legacy, Poison.decode!(value))
+  end
+
+
+  def decode_value(flag, value) when (@str_flag &&& flag) === @str_flag do
+    decode_value(flag ^^^ @str_flag, value)
+  end
+  def decode_value(flag, value) when (flag &&& @str_flag_legacy) === @str_flag_legacy do
+    decode_value(flag ^^^ @str_flag_legacy, value)
+  end
+
 
   def decode_value(_, value), do: value
 
